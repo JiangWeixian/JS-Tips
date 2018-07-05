d36252f118a499f49ab8805f6b0c6d853799c0a0
JiangWeixian <saber_wei@163.com>
2018-07-05 11:17:03 +0800

    CSS权威指南：float

--- Demos/gulpfile.js
+++ Demos/gulpfile.js
@@ -1,29 +1,32 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var stylus = require('gulp-stylus');
+var path = require('path')
+
+var baseDir = 'Slider';
 
 var config = {
-  baseDir: '柱状图',
-  stylusPath: '柱状图/static/stylus/*.styl',
-  watchFiles: [ '柱状图/*.html', '柱状图/static/css/*.css']
+  stylusPath: baseDir + '/static/stylus/*.styl',
+  htmlPath: baseDir + '/*.html',
+  cssPath: baseDir + '/static/css/*.css'
 }
 
 gulp.task('stylus', function() {
   return gulp.src(config.stylusPath)
   .pipe(stylus())
-  .pipe(gulp.dest('柱状图/static/css'))
+  .pipe(gulp.dest(path.join(baseDir, '/static/css')))
   .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('server', ['stylus'], function() {
   browserSync.init({
-    files: config.watchFiles,
+    files: [config.htmlPath, config.cssPath],
     server: {
-      baseDir: config.baseDir
+      baseDir: baseDir
     }
   });
   gulp.watch(config.stylusPath, ['stylus'])
-  gulp.watch('柱状图/*.html').on('change', browserSync.reload);
+  gulp.watch(config.htmlPath).on('change', browserSync.reload);
 
 })
 gulp.task('default',['server']);
